3a6f12e7588dc3329d2e781bc4a6daffb7753e45
phap0208 <92504836+phap0208@users.noreply.github.com>
2022-10-12 20:31:06 +0700

    Add files via upload

---
+++ src/PizzaShape/Circle.java
@@ -0,0 +1,18 @@
+package PizzaShape;
+
+public class Circle implements IShape {
+    private double _radius;
+    public Circle(double r) {
+        _radius = r;
+    }
+    public double getArea() {
+        return Math.PI * _radius * _radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "_radius=" + _radius +
+                '}';
+    }
+}
